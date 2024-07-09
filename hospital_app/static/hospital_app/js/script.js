4d206a21ff63ba486e074d338a9890a0c387765a
RaviKiran25052 <ravivarma25052@gmail.com>
2024-07-09 07:41:42 +0530

    patientAppointments

--- hospital_app/static/hospital_app/js/script.js
+++ hospital_app/static/hospital_app/js/script.js
@@ -60,14 +60,6 @@ function filterDoctors() {
         }
     }
 }
-function viewInvoice(className) {
-    document.querySelector(className).style.display = 'flex';
-    document.querySelector('body').style.overflow = 'auto';
-}
-function closeInvoice(className) {
-    document.querySelector(className).style.display = 'none';
-    document.querySelector('body').style.overflow = 'hidden';
-}
 function openPopup() {
     const popupWindow = document.getElementById("popupWindow");
     popupWindow.style.display = "flex";
@@ -121,5 +113,10 @@ document.getElementById('user-input').addEventListener('keypress', function (e) 
     }
 });
 
-
-
+const appStatusClass = document.querySelector('.appStatus');
+const appStatus = appStatusClass.innerHTML;
+if (appStatus === 'Booked') {
+    appStatusClass.classList.add();
+} else {
+    appStatusClass.classList.add();
+}
